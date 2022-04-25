aed37da6ad2f8d7d224a26b5460c506d953e0bdb
Nandhu-P <102171569+Nandhu-P@users.noreply.github.com>
2022-04-25 09:01:53 +0530

    Create firehouse.h

---
+++ 3_Implementation/inc/firehouse.h
@@ -0,0 +1,11 @@
+
+#ifndef __FIREHOUSE_H__
+#define __FIREOUSE_H__
+#define trigpin 6
+#define echopin 5
+int led1 = A0;
+int led2 = A1;
+int led3 = A2;
+int led4 = A3;
+int led5 = A4;
+#endif /** __FIREHOUSE_H_ */
